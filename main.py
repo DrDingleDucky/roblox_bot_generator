2714cdb8d5fb97c3f161a75fb5b86ad7333cd732
George Davidson <gd240673@gmail.com>
2023-01-22 13:39:31 -0500

    Update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-import options
+import user_options
 
 # The location of your driver.
 PATH = os.path.join("Driver", "chromedriver.exe")
@@ -18,7 +18,9 @@ options = Options()
 options.add_argument("log-level=3")
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 options.add_experimental_option("detach", True)
-# options.add_extension(os.path.join("Solver", "ext.crx"))
+
+if user_options.auto_solve_capta:
+    options.add_extension(os.path.join("Solver", "ext.crx"))
 
 service = Service(executable_path=PATH)
 driver = webdriver.Chrome(service=service, options=options)
@@ -28,10 +30,12 @@ driver.implicitly_wait(1)  # Waiting for the website to fully load.
 
 
 def random_month():
+    # return random.choice(["F", "A"])
     return random.choice(["J", "F", "M", "A", "MM", "JJ", "JJJ", "AA", "S", "O", "N", "D"])
 
 
 def random_day():
+    # return random.choice(["30", "31"])
     return random.choice(["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"])
 
 
@@ -40,6 +44,7 @@ def random_year():
 
 
 def random_username():
+    # return random.choice(["OGlek00AgfmNLyPoJBkc", "mKFuC6E8Hht44LiYVXpO", "0dCl0VBFuQcGgDlbEr8x"])
     return "".join(random.choice(string.digits + string.ascii_letters) for _ in range(20))
 
 
@@ -52,6 +57,46 @@ def random_gender():
 
 
 def credentials_validation_checker():
+    try:
+        if driver.find_element(By.XPATH, '//*[@id="signup-BirthdayInputValidation"]').text == "Invalid birthday.":
+            print("This username is already in use.")
+            return False
+        else:
+            pass
+    except:
+        pass
+
+    try:
+        if driver.find_element(By.XPATH, '//p[@id="signup-usernameInputValidation"]').text == "This username is already in use.":
+            print("This username is already in use.")
+            return False
+        else:
+            pass
+    except:
+        pass
+
+    try:
+        if driver.find_element(By.XPATH, '//p[@id="signup-usernameInputValidation"]').text == "Username not appropriate for Roblox.":
+            print("Username not appropriate for Roblox.")
+            return False
+        else:
+            pass
+    except:
+        pass
+
+    try:
+        if driver.find_element(By.XPATH, '//p[@id="signup-usernameInputValidation"]').text == "Username might contain private information.":
+            print("Username might contain private information.")
+            return False
+        else:
+            pass
+    except:
+        pass
+
+    return True
+
+
+def sign_up():
     try:
         sign_up = driver.find_element(
             By.XPATH, '//*[@id="signup-button"]')
@@ -75,60 +120,71 @@ def error_validation_checker():
 
 def fill_info():
     month_element = driver.find_element(
-        By.XPATH, "//select[@id='MonthDropdown']")
+        By.XPATH, '//select[@id="MonthDropdown"]')
     month_element.click()
     month = random_month()
     month_element.send_keys(month)
 
     day_element = driver.find_element(
-        By.XPATH, "//select[@id='DayDropdown']")
+        By.XPATH, '//select[@id="DayDropdown"]')
     day_element.click()
     day = random_day()
     day_element.send_keys(day)
 
     year_element = driver.find_element(
-        By.XPATH, "//select[@id='YearDropdown']")
+        By.XPATH, '//select[@id="YearDropdown"]')
     year_element.click()
     year = random_year()
     year_element.send_keys(year)
 
     username_element = driver.find_element(
-        By.XPATH, "//input[@id='signup-username']")
+        By.XPATH, '//input[@id="signup-username"]')
     username_element.clear()
     username_element.click()
     username = random_username()
     username_element.send_keys(username)
 
     password_element = driver.find_element(
-        By.XPATH, "//input[@id='signup-password']")
+        By.XPATH, '//input[@id="signup-password"]')
     password_element.clear()
     password_element.click()
     password = random_password()
     password_element.send_keys(password)
 
     gender = driver.find_element(
-        By.XPATH, f"//*[@id='{random_gender()}']")
+        By.XPATH, f'//*[@id="{random_gender()}"]')
     gender.click()
+
+    return f"{username}:{password}\n"
 
 
 def main():
-    while True:
-        fill_info()
+    for _ in range(user_options.number_of_accounts):
+        while True:
+            account_info = fill_info()
+
+            time.sleep(0.5)
+
+            if credentials_validation_checker():
+                print("Good Credentials")
+                break
+            else:
+                print("Bad Credentials, Retrying...")
 
         time.sleep(0.5)
 
-        if credentials_validation_checker():
-            print("Good Credentials")
-            break
+        sign_up()
+
+        if error_validation_checker():
+            print("SUCCESSFUL, There was no unknown error")
         else:
-            print("Bad Credentials")
+            print("FUCK, There was an unknown error. Retry in 45 minutes.")
+            break
 
-    time.sleep(0.5)
+        with open(os.path.join("info", "accounts.txt"), "a") as file:
+            file.write(account_info)
 
-    if error_validation_checker():
-        print("Successful")
-    else:
-        print("FUCK, There was an unknown error. Retry in 45 minutes.")
+    print("Done")
 
 
 if __name__ == "__main__":
